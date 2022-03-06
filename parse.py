7d6e12c4a3ec93bcb6a94a925b7def36942ce00e
Ian Hiew <pixdo.et@gmail.com>
2022-03-06 12:16:05 +0800

    Added colour printing to parse.py

--- parse.py
+++ parse.py
@@ -1,6 +1,18 @@
 # import json
 from datetime import datetime
 
+# colour palette for printing bri'ish text
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    FAIL = '\033[91m'
+    UNDERLINE = '\033[4m'
+    STOP = '\033[0m'
+
+
 def parse(data):
     """
     Parse the data coming from fetch.py
@@ -8,10 +20,12 @@ def parse(data):
     postNumber = data["data"]["children"]
     for post in postNumber:
         postDate = datetime.utcfromtimestamp(post["data"]["created"])
-        print(f"{post['data']['title']} \n")
-        print(f"Link: {post['data']['url']}\n")
+        print(f"{bcolors.YELLOW}{post['data']['title']}{bcolors.STOP} \n")
+        print(f"Link: {bcolors.UNDERLINE}{post['data']['url']}{bcolors.STOP}\n")
         print(
-            f"Posted on {post['data']['subreddit_name_prefixed']} by u/{post['data']['author']} on {postDate} ")
+            f"Posted on {bcolors.PURPLE}{post['data']['subreddit_name_prefixed']}{bcolors.STOP} by {bcolors.BLUE}u/{post['data']['author']}{bcolors.STOP} on {postDate}")
         print("---------------------")
 
     print(f"{len(postNumber)} posts shown.")
+
+
